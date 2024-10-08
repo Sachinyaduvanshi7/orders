8b03fd174730f47d4f75db0e8c71d0fc39697bf1
Sachinyaduvanshi7 <sachinyrao7@gmail.com>
2024-10-08 16:41:57 +0530

    bbb

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3001;
 const app = express();
 
 app.get("", (req, res) => {
-  res.send("Message from order service-3");
+  res.send("Message from order service-5");
 });
 
 app.listen(PORT, () => console.log("ORDER SERVICE STARTED"));
